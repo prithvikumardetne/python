e4c702027258044cc995b052fc8e262f36c7c0ce
Prithvi Kumar Detne <38846173+prithvikumardetne@users.noreply.github.com>
2024-10-18 18:06:09 +0000

    committing some more programs

--- math_1.py
+++ math_1.py
@@ -1,3 +1,5 @@
 import math
 
 print(math.factorial(6))
+
+print(math.factorial(9))
---
+++ string_sort_2.py
@@ -0,0 +1,11 @@
+str = "My Name is Prithvi Kumar Detne"
+
+
+str_list = list(str)
+
+print(str_list)
+
+str_list_updated = []
+for i in str.split():
+    str_list_updated.append(i)
+
