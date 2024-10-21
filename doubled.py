1f8dbbcfb29efa588295a59521318ed777373413
Prithvi Kumar Detne <38846173+prithvikumardetne@users.noreply.github.com>
2024-10-21 19:51:11 +0000

    multiplication program

---
+++ doubled.py
@@ -0,0 +1,5 @@
+numbers = [1,2,3,4]
+
+doubled_numbers = [num*2 for num in numbers]
+
+print(doubled_numbers)
---
+++ evennumbers.py
@@ -0,0 +1,7 @@
+even_numbers = [i for i in range(1,20) if i % 2 == 0]
+
+print(even_numbers)
+
+multiplication = [[i*j for j in range(1,10)] for i in range(2,5)]
+
+print(multiplication)
