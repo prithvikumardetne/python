0b2758988110a1adc7ae9a0902a4804e8340dcd9
Prithvi Kumar Detne <38846173+prithvikumardetne@users.noreply.github.com>
2024-10-22 16:16:18 +0000

    committing extend and sorted

---
+++ extend.py
@@ -0,0 +1,11 @@
+numbers1 = [1,2,3,4,5]
+
+numbers2 = [10,20,30]
+
+numbers1.extend(numbers2)
+
+numbers2.extend(numbers1)
+
+print(numbers1)
+
+print(numbers2)
---
+++ sorted_numbers.py
@@ -0,0 +1,15 @@
+numbers = [5,1,8,6,9]
+
+sorted_numbers = sorted(numbers)
+
+print(sorted_numbers)
+
+sorted_numbers_reverse = sorted(numbers,reverse=True)
+
+print(sorted_numbers_reverse)
+
+fruits = ['Grapes','apple','orange','banana','Mango']
+
+sorted_fruits = sorted(fruits,key=len)
+
+print(sorted_fruits)
