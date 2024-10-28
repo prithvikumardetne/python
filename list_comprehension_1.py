5fa827d74ebda3b7d83f2819591ee8753a4c5e67
Prithvi Kumar Detne <38846173+prithvikumardetne@users.noreply.github.com>
2024-10-28 14:25:28 -0500

    list comprehensions

---
+++ list_comprehension_1.py
@@ -0,0 +1,5 @@
+numbers = []
+
+numbers = [2**i for i in range(1,6)]
+
+print(numbers)
---
+++ list_comprehension_2.py
@@ -0,0 +1,7 @@
+name1 = [1,2,3]
+
+name2 = [4,5,6]
+
+final_names = [(i,j) for i in name1 for j in name2]
+
+print(final_names)
